ba9e60271324d69e16a7c16e54e4aeed316b31df
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-08-15 23:29:09 +0530

    twitter widget fix

--- src/components/pieces/TwitterHandle.js
+++ src/components/pieces/TwitterHandle.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { makeStyles, Typography } from "@material-ui/core";
-import { Timeline } from "react-twitter-widgets";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		padding: theme.spacing(2),
+	},
+	tweetDiv: {
+		height: 500,
+		overflow: "auto",
 	},
 }));
 
@@ -16,15 +19,17 @@ const TwitterHandle = () => {
 			<Typography variant="h6" gutterBottom>
 				{`Announcements`}
 			</Typography>
-			<Timeline
-				dataSource={{
-					sourceType: "profile",
-					screenName: "IndianSpeechCo1",
-				}}
-				options={{
-					height: "500",
-				}}
-			/>
+			<div className={classes.tweetDiv}>
+				<a
+					className="twitter-timeline"
+					data-lang="en"
+					data-height="500"
+					data-dnt="true"
+					href="https://twitter.com/IndianSpeechCo1?ref_src=twsrc%5Etfw"
+				>
+					Tweets by IndianSpeechCo1
+				</a>
+			</div>
 		</div>
 	);
 };
